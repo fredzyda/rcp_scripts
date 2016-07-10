9401a19d446d308a1bbe717feaca19961adf85c5
Fred Zyda on his trusty MacBook Pro <fredzyda@mac.com>
2016-07-10 15:03:19 -0700

    added gear ratio computation.
    The supra has a speed sensor at the transmission output shaft, so
    I'm just using that to compute current gear ratio directly. I plan
    to later use this information to figure out what gear I'm in. This
    is the way easier way to do things relative to using GPS to guess the
    current gear ratio...

--- rcp.lua
+++ rcp.lua
@@ -4,12 +4,23 @@ tickRate = 10
 --actually set the tick rate
 setTickRate(tickRate)
 
+-- variables for automatic logging stop/start
 -- a counter for stopping the logging
 shutdownCounter = 0
 -- how long after shutdown should we keep logging? in seconds.
 shutdownTime = 30
 -- are we actually logging? keep track.
 isLogging = false
+
+-- variables for gear ratio calculations
+-- set up the virtual channel
+-- I know the supra only has ratios from -3.768 to 3.285
+ratioId = addChannel("GearRatio", 10, 3, -5, 5)
+-- reverse is a distinct ratio, so I can detect it
+reverseRatio = 3.768
+-- when using computed gear ratio for computation, how close
+-- do we have to be before we call it close enough?
+ratioTolerance = 0.15
 
 function doLogging()
     local gps = getGpsQuality()
@@ -34,6 +45,29 @@ function doLogging()
     end
 end
 
+function doGearRatio()
+    -- the supra has a transmission output shaft speed sensor.
+    -- I have this sensor wired to timer 1, so gear ratio calculation is easy
+    local engineRpm = getTimerRpm(0)
+    local driveshaftRpm = getTimerRpm(1)
+    local ratio = 0
+
+    if driveshaftRpm > 0 then
+        -- avoid dividing by zero
+        ratio = engineRpm/driveshaftRpm
+    end
+
+    if math.abs(ratio - reverseRatio) < ratioTolerance then
+        -- make the ratio negative if we're in reverse.
+        -- this is probably not something we strictly need, but it is amusing.
+        ratio = -1 * ratio
+    end
+
+    -- set the actual virtual channel output
+    setChannel(ratioId, ratio)
+end
+
 function onTick()
     doLogging()
+    doGearRatio()
 end
