51b4dcd1439e4a95ae66c46c170b1aef2688b7eb
Fred Zyda on his trusty MacBook Pro <fredzyda@mac.com>
2016-07-10 15:19:24 -0700

    Added gear number to the gear ratio calculation!
    Now we can log what gear we're in! Sweet!
    
    There are some known bugs:
    This currently has no idea if you're holding the clutch in or not,
    so as you coast down from a higher gear, you'll appear to briefly
    enter all the lower gears on your way, possibly including reverse.
    I should figure out how to filter for that in some useful way...

--- rcp.lua
+++ rcp.lua
@@ -13,11 +13,20 @@ shutdownTime = 30
 isLogging = false
 
 -- variables for gear ratio calculations
--- set up the virtual channel
+-- set up the virtual channel for gear ratio
 -- I know the supra only has ratios from -3.768 to 3.285
 ratioId = addChannel("GearRatio", 10, 3, -5, 5)
+-- set up the virtual channel for gear number
+-- note that you'd have to change the range if you have more gears
+gearId = addChannel("Gear", 10, 0, -1, 5)
 -- reverse is a distinct ratio, so I can detect it
 reverseRatio = 3.768
+-- gear ratios for all the other gears
+firstRatio = 3.285
+secondRatio = 1.894
+thirdRatio = 1.275
+fourthRatio = 1.00
+fifthRatio = 0.783
 -- when using computed gear ratio for computation, how close
 -- do we have to be before we call it close enough?
 ratioTolerance = 0.15
@@ -57,14 +66,32 @@ function doGearRatio()
         ratio = engineRpm/driveshaftRpm
     end
 
+    local gearNum = 0
+
+    -- now use this computed ratio to figure out what gear we're in.
     if math.abs(ratio - reverseRatio) < ratioTolerance then
         -- make the ratio negative if we're in reverse.
         -- this is probably not something we strictly need, but it is amusing.
         ratio = -1 * ratio
+        gearNum = -1
+    elseif math.abs(ratio - firstRatio) < ratioTolerance then
+        gearNum = 1
+    elseif math.abs(ratio - secondRatio) < ratioTolerance then
+        gearNum = 2
+    elseif math.abs(ratio - thirdRatio) < ratioTolerance then
+        gearNum = 3
+    elseif math.abs(ratio - fourthRatio) < ratioTolerance then
+        gearNum = 4
+    elseif math.abs(ratio - fifthRatio) < ratioTolerance then
+        gearNum = 5
+    else
+        -- we must be coasting or stopped...
+        gearNum = 0
     end
 
-    -- set the actual virtual channel output
+    -- set the actual virtual channel outputs
     setChannel(ratioId, ratio)
+    setChannel(gearId, gearNum)
 end
 
 function onTick()
